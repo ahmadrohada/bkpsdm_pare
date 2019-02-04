1a19ec77a1962d4d4af3ab24e483279d503230e2
Ahmad Rohada <rohada987@gmail.com>
2019-02-04 16:25:14 +0700

    undo lagi

---
+++ app/Models/SKPD.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SKPD extends Model
+{
+
+    protected $connection = 'mysql2';
+    protected $table = 'm_skpd';
+
+   /*  protected $fillable = [
+        'id', 'unit_kerja','id_skpd','skpd'
+    ]; */  
+
+    public function UnitKerja()
+    {
+        return $this->hasMany('App\Models\Skpd','parent_id','id');
+    }
+
+    public function bidang()
+    {
+        return $this->hasOne('App\Models\UnitKerja','id','id');
+    }
+
+   
+
+   
+}
