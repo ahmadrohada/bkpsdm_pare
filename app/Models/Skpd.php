e1f32f08f949cf61c7437c074789dd18da3836a6
Ahmad Rohada <rohada987@gmail.com>
2019-02-23 10:43:53 +0700

    tes

---
+++ app/Models/Skpd.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Skpd extends Model
+{
+
+    protected $connection = 'mysql2';
+    protected $table = 'm_skpd';
+
+
+    public function UnitKerja()
+    {
+        return $this->hasMany('App\Models\Skpd','parent_id','id');
+    }
+
+    public function bidang()
+    {
+        return $this->hasOne('App\Models\UnitKerja','id','id');
+    }
+
+    public function pejabat()
+    {
+        return $this->hasOne('App\Models\HistoryJabatan','id_jabatan')->where('status','active');;
+    }
+
+   
+
+   
+}
