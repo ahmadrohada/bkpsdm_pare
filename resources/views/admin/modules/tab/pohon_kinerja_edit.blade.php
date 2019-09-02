6607264d8eca63d4b6825efff672505779618c55
Ahmad Rohada <rohada987@gmail.com>
2019-09-02 19:38:25 +0700

    tes all

--- resources/views/admin/modules/tab/pohon_kinerja_edit.blade.php
+++ resources/views/admin/modules/tab/pohon_kinerja_edit.blade.php
@@ -18,7 +18,7 @@
 		@include('admin.tables.pohon_kinerja-ind_kegiatan_edit')
 		@include('admin.tables.pohon_kinerja-ind_kegiatan2_edit')
 		
-
+ 
 	</div>
 </div>
 
@@ -42,11 +42,6 @@
 				'data' : {
 						"url" 	: "{{ url("api_resource/skpd_renja_aktivity") }}",
 						"data" 	: function (node) {
-
-							
-							
-							
-						
 							return { "renja_id" : {!! $renja->id !!} };
 						},
 						"dataType" : "json"
@@ -56,10 +51,110 @@
 							'responsive' : false
 						}
 			}
+			,'contextmenu' : {
+					'items' : context_menu
+				},
+				'plugins' : [/* 'contextmenu', */ 'types' ,'search'],
+				'types' : {
+					'tujuan' 		: { /* options */ },
+					'ind_tujuan' 	: { /* options */ },
+					'sasaran' 		: { /* options */ },
+					'ind_sasaran' 	: { /* options */ },
+					'program' 		: { /* options */ },
+					'ind_program' 	: { /* options */ },
+					'kegiatan' 		: { /* options */ },
+					'ind_kegiatan' 	: { /* options */ }
+				}
 			
-		}).on("loaded.jstree", function(){
+		
+		}).on('create_node.jstree', function (e, data) {
+			var text = data.node.parent;
+			var tx = text.split('|');
+
+
+
+			switch ( tx[0] ){
+				case 'ind_tujuan':
+					var url = 'api_resource/sasaran_store'
+				break;
+				case 'sasaran':
+					var url = 'api_resource/ind_sasaran_store'
+				break;
+				case 'ind_sasaran':
+					var url = 'api_resource/program_store'
+				break;
+				case 'program':
+					var url = 'api_resource/ind_program_store'
+				break;
+				case 'ind_program':
+					var url = 'api_resource/kegiatan_store'
+				break;
+				case 'kegiatan':
+					var url = 'api_resource/ind_kegiatan_store'
+				break;
+
+				default:
+					var url = 'default_url';
+				
+			}
+
+
+			$.post( '{!! url("'+url+'") !!}', 
+				{ 	'parent'	: tx[0],
+					'parent_id' : tx[1], 
+					'renja_id' 	: {!! $renja->id !!},
+					'position' 	: data.position, 
+					'text' 		: data.node.text 
+				})
+				.done(function (d) {
+					data.instance.set_id(data.node, d.id);
+				})
+				.fail(function () {
+					data.instance.refresh();
+				});
+		}).on('rename_node.jstree', function (e, data) {
+
+			var z = data.node.id;
+			var dt = z.split('|');
+
+
+
+			switch ( dt[0] ){
+				case 'sasaran':
+					var url = 'api_resource/sasaran_rename'
+				break;
+				case 'ind_sasaran':
+					var url = 'api_resource/ind_sasaran_rename'
+				break;
+				case 'program':
+					var url = 'api_resource/program_rename'
+				break;
+				case 'ind_program':
+					var url = 'api_resource/ind_program_rename'
+				break;
+				case 'kegiatan':
+					var url = 'api_resource/kegiatan_rename'
+				break;
+				case 'ind_kegiatan':
+					var url = 'api_resource/ind_kegiatan_rename'
+				break;
+
+				default:
+					var url = 'default_url';
+				
+			}
+
+			$.post( '{!! url("'+url+'") !!}', 
+			 		{ 	
+						'id' 	: dt[1], 
+						'text' 	: data.text 
+					})
+				.fail(function () {
+					data.instance.refresh();
+				});
+		})
+		.on("loaded.jstree", function(){
 			$('#renja_tree_kegiatan').jstree('open_all');
-			
 		})
 		.on("changed.jstree", function (e, data) {
 			if(data.selected.length) {
@@ -68,6 +163,81 @@
 				detail_table(data.instance.get_node(data.selected[0]).id);
 			}
 		});
+	}
+
+
+	function context_menu(node){
+		var tree = $('#renja_tree_kegiatan').jstree(true);
+
+		if (node.type === 'ind_tujuan'){
+			var addLabel = 'Tambah Sasaran';
+			var newLabel = 'Sasaran Renja';
+		}else if ( node.type === 'sasaran'){
+			var addLabel = 'Tambah Indikator';
+			var newLabel = 'Indikator Sasaran Renja';
+		}else if ( node.type === 'ind_sasaran'){
+			var addLabel = 'Tambah Program';
+			var newLabel = 'Program Renja';
+		}else if ( node.type === 'program'){
+			var addLabel = 'Tambah Indikator';
+			var newLabel = 'Indikator Program Renja';
+		}else if ( node.type === 'ind_program'){
+			var addLabel = 'Tambah Kegiatan';
+			var newLabel = 'Kegiatan Renja';
+		}else if ( node.type === 'kegiatan'){
+			var addLabel = 'Tambah Indikator';
+			var newLabel = 'Indikator Kegiatan Renja';
+		}else{
+			var addLabel = 'Tambah Data';
+			var newLabel = 'Data Renja';
+		}
+	
+		// The default set of all items
+		var items = {
+			"Add": {
+				"label": addLabel,
+				"action": function (obj) { 
+					var $node = tree.create_node(node,newLabel);
+					tree.edit($node);
+
+					
+					
+				}
+			},
+			"Rename": {
+				"label": "Rename",
+				"action": function (obj) { 
+					tree.edit(node);
+				}
+			},
+			"Edit": {
+				"label": "Edit",
+				"action": function (obj) { 
+					//tree.edit(node);
+				}
+			},                         
+			"Remove": {
+				"label": "Remove",
+				"action": function (obj) { 
+					if(confirm('Are you sure to remove this category?')){
+						tree.delete_node(node);
+					}
+				}
+			}
+		};
+
+
+
+		if (node.type === 'tujuan'){
+			delete items.Add;
+			delete items.Rename;
+			delete items.Edit;
+			delete items.Remove;
+		}else if ( node.type === 'ind_kegiatan'){
+			delete items.Add;
+		}
+	
+		return items;
 	}
 
 
@@ -135,7 +305,7 @@
 							$(".div_ind_sasaran_detail, .div_program_list").show();
 							$(".div_program_detail, .div_ind_program_list").hide();
 							$(".div_ind_program_detail, .div_kegiatan_list").hide();
-							$(".div_kegiatan_detail,.div_ind_kegiatan_list").hide();
+							$(".div_kegiatan_detail, .div_ind_kegiatan_list").hide();
 							$(".div_ind_kegiatan_detail").hide();
 							load_program( tx[1]);
 					
@@ -148,7 +318,7 @@
 							$(".div_ind_sasaran_detail, .div_program_list").hide();
 							$(".div_program_detail, .div_ind_program_list").show();
 							$(".div_ind_program_detail, .div_kegiatan_list").hide();
-							$(".div_kegiatan_detail,.div_ind_kegiatan_list").hide();
+							$(".div_kegiatan_detail, .div_ind_kegiatan_list").hide();
 							$(".div_ind_kegiatan_detail").hide();
 							load_ind_program( tx[1]);
 					
