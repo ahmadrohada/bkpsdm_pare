b379f2167568c52c9bcbb133791692d148f7dd1d
Ahmad Rohada <rohada987@gmail.com>
2019-09-06 00:54:18 +0700

    tes

--- resources/views/admin/tables/administrator-pohon_kinerja.blade.php
+++ resources/views/admin/tables/administrator-pohon_kinerja.blade.php
@@ -1,7 +1,7 @@
 <div class="box {{ $h_box }}">
     <div class="box-header with-border">
         <h1 class="box-title">
-            Pohon Kinerja SKPD
+            Pohon Kinerja SKPD 
         </h1>
 
         <div class="box-tools pull-right">
