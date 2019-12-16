d12f7f3068ac9fae5fcc66b1cae6889093a9efca
Ahmad Rohada <rohada987@gmail.com>
2019-12-16 22:27:57 +0700

    uptd PUPR

--- app/Http/Controllers/API/KegiatanAPIController.php
+++ app/Http/Controllers/API/KegiatanAPIController.php
@@ -239,6 +239,11 @@ class KegiatanAPIController extends Controller {
                 $level2 = SKPD::whereRaw('(parent_id = ? and  id != ? ) or parent_id = ? ', array(650,675,675))
                                 ->select('id','skpd')
                                 ->get();
+            }else if ( $x->id == '273'){  //PUPR
+               
+                $level2 = SKPD::whereRaw('(parent_id = ? and  id != ? ) or parent_id = ? ', array(273,302,302))
+                                ->select('id','skpd')
+                                ->get();
             }else if ( $x->id == '740'){  //disperindag
                
                 $level2 = SKPD::whereRaw('(parent_id = ? and  id != ? ) or parent_id = ? ', array(740,761,761))
