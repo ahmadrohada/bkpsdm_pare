afe7ede10d4998a2b20612a76d6a98fc7fdce6ea
Ahmad Rohada <rohada987@gmail.com>
2019-12-26 23:18:10 +0700

    update printout perjakin

--- resources/views/admin/printouts/cetak_perjanjian_kinerja-Eselon2.blade.php
+++ resources/views/admin/printouts/cetak_perjanjian_kinerja-Eselon2.blade.php
@@ -3,10 +3,134 @@
 		@include('admin.printouts.style')
 
 
-		<title>Cetak TPP Report - Eselon II</title>
+		<title>Cetak TPP Report</title>
 </head>
 <body>
+
+
+	<div style="padding:5px 50px 10px 50px;">
+
+
+	<table width="100%" style="margin-top:20px;">
+		<tr>
+			<td align="center" valign="top">
+				<img src="{{asset('assets/images/logo_jabar.png')}}" width="100px" height="120px" >
+			</td>
+		</tr>
+		<tr>
+			<td align="center" style="padding:20px 0 35px 0 ;">
+				<FONT style="font-size:13pt; font-family:Cambria; letter-spacing:1pt;">
+					PERJANJIAN KINERJA TAHUN {{ $periode }}
+				</FONT>
+			</td>
+		</tr>
+	</table>
+
 	
+
+		<p class="isi_text">
+			Dalam rangka mewujudkan manajemen pemerintahan yang efektif, transparan dan 
+			akuntabel serta berorientasi pada hasil, kami yang bertanda tangan dibawah ini :
+		</p>
+		
+		<table  width="100%" style="margin-left:-2px; margin-top:17px;">
+				<tr>
+					<td width="15%">Nama</td><td width="2%">:</td><td width="" align="left">{{ $nama_pejabat }}</td>
+				</tr>
+				<tr>
+					<td>Jabatan</td><td>:</td><td>{{ $jabatan }}</td>
+				</tr>
+			</table>
+			
+			<p style="margin-top:5px;">Selanjutnya disebut PIHAK KESATU</p>
+		
+			
+			<table  width="100%" style="margin-left:-2px; margin-top:20px;">
+				<tr>
+					<td width="15%">Nama</td><td width="2%">:</td><td width="" align="left">RIDWAN KAMIL</td>
+				</tr>
+				<tr>
+					<td>Jabatan</td><td>:</td><td>GUBERNUR JAWA BARAT</td>
+				</tr>
+			</table>
+			<p style="margin-top:5px;">Selaku atasan PIHAK KESATU, selanjutnya disebut PIHAK KEDUA</p>
+		
+			
+		
+		
+			<p class="isi_text" style="margin-top:35px;">
+			PIHAK KESATU berjanji akan mewujudkan target kinerja yang seharusnya sesuai
+			perjanjian ini, dalam rangka mewujudkan target kinerja jangka menengah seperti yang
+			telah ditetapkan dalam dokumen perencanaan. Keberhasilan dan kegagalan pencapaian target
+			kinerja tersebut menjadi tanggung jawab kami.
+			<br>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			PIHAK KEDUA akan memberikan supervisi yang diperlukan serta akan melakukan
+			evaluasi terhadap capaian kinerja dari perjanjian ini dan mengambil tindakan yang diperlukan
+			dalam rangka pemberian penghargaan dan sanksi.
+			</p>
+		
+		
+			<table  class="sign_report" width="100%" style="margin-top:70px;">
+				<tr>
+					<td width="40%">
+						
+					</td>
+					<td width="20%">
+						
+					</td>
+					<td width="40%">
+						Bandung, {{ Pustaka::balik2($tgl_dibuat) }}
+					</td>
+				</tr>
+				<tr height="90px">
+						<td>
+							PIHAK KEDUA,
+						</td>
+						<td>
+							
+						</td>
+						<td>
+							PIHAK KESATU,
+						</td>
+					</tr>
+					<tr height="150px">
+						<td height="60px">
+							
+						</td>
+						<td>
+							
+						</td>
+						<td>
+							
+						</td>
+					</tr>
+					
+					<tr>
+						<td>
+						RIDWAN KAMIL
+						
+						</td>
+						<td>
+							
+						</td>
+						<td>
+						<u>{{ $nama_pejabat }}</u><br>
+						{{ $jenis_jabatan }}<br>
+						<font style="font-size:10pt;">NIP. {{ $nip_pejabat }}</font>
+						</td>
+					</tr>
+				
+				</table>
+	</div>
+
+	
+		
+
+	<pagebreak></pagebreak>
+
+
+
 		<table class="kop" border="0">
 				<tr>
 					<td rowspan="4" align="right" valign="top">
@@ -65,7 +189,7 @@
 				<tr>
 					<td colspan="2"  valign="top" align="center">
 						<FONT style=" font-size:11pt;  font-family:Times New Roman,Cambria;">
-							{{ $jabatan }}
+							KEPALA {{ $nama_skpd }}
 						</font>
 					</td>
 				</tr>
@@ -81,8 +205,8 @@
 		<thead>
 			<tr>
 				<th width="5%" >NO</th>
-				<th width="">SASARAN STRATEGIS/PROGRAM</th>
-				<th width="35%">INDIKATOR PROGRAM</th>
+				<th width="">SASARAN STRATEGIS/SASARAN</th>
+				<th width="35%">INDIKATOR KINERJA</th>
 				<th width="15%" >TARGET</th>
 															
 			</tr>
@@ -94,8 +218,8 @@
 
 				<tr>
                     <td align='right'>{{ $i++ }}</td>
-					<td>{{ $p->sasaran_label ." / ". $p->program_label }}</td>
-					<td>{{ $p->ind_program_label }}</td>
+					<td>{{ $p->sasaran_label }}</td>
+					<td>{{ $p->ind_sasaran_label }}</td>
 					<td align='center'>{{ $p->target." ".$p->satuan  }}</td>
                 </tr>
 			
@@ -107,7 +231,7 @@
 		<thead>
 			<tr>
 				<th width="5%" >NO</th>
-				<th width="">KEGIATAN</th>
+				<th width="">PROGRAM / KEGIATAN</th>
 				<th width="18%">ANGGARAN</th>
 				<th width="20%" > KETERANGAN </th>
 															
@@ -120,7 +244,7 @@
 
 				<tr>
                     <td align='right'>{{ $i++ }}</td>
-					<td>{{ $x->kegiatan_label }}</td>
+					<td>{{ $x->program_label }}</td>
 					<td align='right'>Rp. {{ number_format( $x->anggaran, '0', ',', '.') }}</td>
 					<td></td>
                 </tr>
@@ -157,7 +281,7 @@
 			
 		</td>
 		<td>
-			{{ Pustaka::capital_string($jabatan) }}
+			Kepala {{ Pustaka::capital_string($nama_skpd) }}
 		</td>
 	</tr>
 	<tr height="150px">
@@ -181,9 +305,9 @@
 			
 		</td>
 		<td>
-		<u>{{ $nama_pejabat }}</u><br>
-		{{ $jenis_jabatan }}<br>
-		<font style="font-size:10pt;">NIP. {{ $nip_pejabat }}</font>
+		<u>{{ $nama_ka_skpd }}</u><br>
+		{{ $jenis_jabatan_ka_skpd }}<br>
+		<font style="font-size:10pt;">NIP. {{ $nip_ka_skpd }}</font>
 		</td>
 	</tr>
 
--- resources/views/admin/printouts/cetak_perjanjian_kinerja-Eselon4.blade.php
+++ resources/views/admin/printouts/cetak_perjanjian_kinerja-Eselon4.blade.php
@@ -7,6 +7,126 @@
 </head>
 <body>
 	
+	<div style="padding:5px 50px 10px 50px;">
+
+
+		<table width="100%" style="margin-top:20px;">
+			<tr>
+				<td align="center" valign="top">
+					<img src="{{asset('assets/images/logo_jabar.png')}}" width="100px" height="120px" >
+				</td>
+			</tr>
+			<tr>
+				<td align="center" style="padding:20px 0 35px 0 ;">
+					<FONT style="font-size:13pt; font-family:Cambria; letter-spacing:1pt;">
+						PERJANJIAN KINERJA TAHUN {{ $periode }}
+					</FONT>
+				</td>
+			</tr>
+		</table>
+	
+		
+	
+			<p class="isi_text">
+				Dalam rangka mewujudkan manajemen pemerintahan yang efektif, transparan dan 
+				akuntabel serta berorientasi pada hasil, kami yang bertanda tangan dibawah ini :
+			</p>
+			
+			<table  width="100%" style="margin-left:-2px; margin-top:17px;">
+					<tr>
+						<td width="15%">Nama</td><td width="2%">:</td><td width="" align="left">{{ $nama_pejabat }}</td>
+					</tr>
+					<tr>
+						<td>Jabatan</td><td>:</td><td>{{ $jabatan }}</td>
+					</tr>
+				</table>
+				
+				<p style="margin-top:5px;">Selanjutnya disebut PIHAK KESATU</p>
+			
+				
+				<table  width="100%" style="margin-left:-2px; margin-top:20px;">
+					<tr>
+						<td width="15%">Nama</td><td width="2%">:</td><td width="" align="left">RIDWAN KAMIL</td>
+					</tr>
+					<tr>
+						<td>Jabatan</td><td>:</td><td>GUBERNUR JAWA BARAT</td>
+					</tr>
+				</table>
+				<p style="margin-top:5px;">Selaku atasan PIHAK KESATU, selanjutnya disebut PIHAK KEDUA</p>
+			
+				
+			
+			
+				<p class="isi_text" style="margin-top:35px;">
+				PIHAK KESATU berjanji akan mewujudkan target kinerja yang seharusnya sesuai
+				perjanjian ini, dalam rangka mewujudkan target kinerja jangka menengah seperti yang
+				telah ditetapkan dalam dokumen perencanaan. Keberhasilan dan kegagalan pencapaian target
+				kinerja tersebut menjadi tanggung jawab kami.
+				<br>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				PIHAK KEDUA akan memberikan supervisi yang diperlukan serta akan melakukan
+				evaluasi terhadap capaian kinerja dari perjanjian ini dan mengambil tindakan yang diperlukan
+				dalam rangka pemberian penghargaan dan sanksi.
+				</p>
+			
+			
+				<table  class="sign_report" width="100%" style="margin-top:70px;">
+					<tr>
+						<td width="40%">
+							
+						</td>
+						<td width="20%">
+							
+						</td>
+						<td width="40%">
+							Bandung, {{ Pustaka::balik2($tgl_dibuat) }}
+						</td>
+					</tr>
+					<tr height="90px">
+							<td>
+								PIHAK KEDUA,
+							</td>
+							<td>
+								
+							</td>
+							<td>
+								PIHAK KESATU,
+							</td>
+						</tr>
+						<tr height="150px">
+							<td height="60px">
+								
+							</td>
+							<td>
+								
+							</td>
+							<td>
+								
+							</td>
+						</tr>
+						
+						<tr>
+							<td>
+							RIDWAN KAMIL
+							
+							</td>
+							<td>
+								
+							</td>
+							<td>
+							<u>{{ $nama_pejabat }}</u><br>
+							{{ $jenis_jabatan }}<br>
+							<font style="font-size:10pt;">NIP. {{ $nip_pejabat }}</font>
+							</td>
+						</tr>
+					
+					</table>
+		</div>
+	
+		
+			
+	
+		<pagebreak></pagebreak>
 		<table class="kop" border="0">
 				<tr>
 					<td rowspan="4" align="right" valign="top">
