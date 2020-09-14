f88c30b39a21ecb16b1075ea4904088bb94b3da2
Ahmad Rohada <rohada987@gmail.com>
2020-09-15 00:56:34 +0700

    disable server side/trial on rencana aksi time table

--- resources/views/pare_pns/tables/skp_tahunan-rencana_aksi_time_table_3.blade.php
+++ resources/views/pare_pns/tables/skp_tahunan-rencana_aksi_time_table_3.blade.php
@@ -44,7 +44,7 @@
 		var table_rencana_aksi = $('#rencana_aksi_time_table').DataTable({
 			destroy			: true,
 			processing      : true,
-			serverSide      : true,
+			serverSide      : false,
 			searching      	: true,
 			paging          : true,
 			autoWidth		: false,
