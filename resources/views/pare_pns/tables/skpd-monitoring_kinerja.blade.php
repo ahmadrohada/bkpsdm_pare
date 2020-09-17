6123bdcbb570bf7c4ead1b6908d38dbeb7327324
Ahmad Rohada <rohada987@gmail.com>
2020-09-18 02:03:28 +0700

    update

--- resources/views/pare_pns/tables/skpd-monitoring_kinerja.blade.php
+++ resources/views/pare_pns/tables/skpd-monitoring_kinerja.blade.php
@@ -138,7 +138,7 @@ table.dataTable tbody td {
 								},
 				targets			: 'no-sort',
 				bSort			: false,
-				rowsGroup		: [ 0,1,4,5,6,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30],
+				rowsGroup		: [ 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30],
 				columns			: [
 									{ data: 'tujuan_label',name:'tujuan_label'},
 									{ data: 'indikator_tujuan_label',name:'indikator_tujuan_label'},
