521fa33b2285ee1da047ad11d78eb3b0ade11723
Ahmad Rohada <rohada987@gmail.com>
2020-12-24 00:45:04 +0700

    update

--- resources/views/pare_pns/tables/personal-capaian_tahunan.blade.php
+++ resources/views/pare_pns/tables/personal-capaian_tahunan.blade.php
@@ -122,7 +122,7 @@
 													//blm dikirim
 													return  	'<span  data-toggle="tooltip" title="Edit" style="margin:2px;" ><a class="btn btn-success btn-xs edit_capaian_tahunan" data-id="'+row.capaian_id+'"><i class="fa fa-pencil" ></i></a></span>'+
 																'<span  data-toggle="tooltip" title="lihat" style="margin:2px;" ><a class="btn btn-default btn-xs "><i class="fa fa-eye" ></i></a></span>'+
-																'<span  data-toggle="tooltip" title="cetak" style="margin:2px;" ><a class="btn btn-primary btn-xs cetak_capaian_tahunan"  data-id="'+row.capaian_id+'"><i class="fa fa-print" ></i></a></span>'+
+																'<span  data-toggle="tooltip" title="cetak" style="margin:2px;" ><a class="btn btn-default btn-xs "  data-id="'+row.capaian_id+'"><i class="fa fa-print" ></i></a></span>'+
 																'<span  data-toggle="tooltip" title="Hapus" style="margin:2px;" ><a class="btn btn-danger btn-xs hapus_capaian_tahunan" data-id="'+row.capaian_id+'"><i class="fa fa-close " ></i></a></span>';
 												}
 											}else{
